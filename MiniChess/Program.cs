cff99258eb4ecf3400d85211aff11d2233e7be08
andrelpoj <andrelpoj@gmail.com>
2018-05-01 16:16:48 -0300

    Criação da classe State

---
+++ MiniChess/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using StateNS;
+
+namespace MiniChess
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i,j;
+            char[,] b = new char[6,6];
+            for(i=0; i<6; i++){
+                for(j=0; j<6; j++){
+                    b[i,j] = '0';
+                }
+            }
+            int cp = 1;
+            int[] lm = {3,2,3,3};
+            State inicial = new State(b,cp,lm);
+            inicial.print();
+        }
+    }
+}
---
+++ MiniChess/State.cs
@@ -0,0 +1,29 @@
+using System;
+
+namespace StateNS
+{
+    class State
+    {
+        char[,] board;
+        int currentPlayer;
+        int[] lastMove;
+
+        public State(char[,] board, int currentPlayer, int[] lastMove){
+            this.board = board;
+            this.currentPlayer = currentPlayer;
+            this.lastMove = lastMove;
+        }
+
+        public void print(){
+            Console.WriteLine($"Current Player:{currentPlayer}");
+            Console.WriteLine("Board:");
+            for(int i=0; i<6; i++){
+                for(int j=0; j<6; j++){
+                    Console.Write($"{board[i,j]} ");
+                }
+                Console.Write("\n");
+            }
+            Console.WriteLine($"LastMove: [{lastMove[0]}][{lastMove[1]}] to [{lastMove[2]}][{lastMove[3]}]");
+        }
+    }
+}
