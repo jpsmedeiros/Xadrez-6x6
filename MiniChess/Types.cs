82aa3aa2036ae047c816fc1950cc708629337f5b
João Pedro Sá Medeiros <jpsmedeiros@id.uff.br>
2018-05-02 08:32:58 -0300

    Metodos de verificacao das peças

--- MiniChess/Types.cs
+++ MiniChess/Types.cs
@@ -10,5 +10,23 @@ namespace TypesNS
         public char BISHOP = 'B';
         public char PAWN = 'P';
         public char EMPTY = '0';
+
+        public bool isPlayer1(char piece){
+            return Char.IsUpper(piece);
+        }
+        public bool isPlayer2(char piece){
+            return Char.IsLower(piece);
+        }
+        public bool isPlayer(char piece, int player){
+            if(player == 1){
+                return isPlayer1(piece);
+            }
+            return isPlayer2(piece);
+        }
+        public bool isEmpty(char piece){
+            if(piece == EMPTY)
+                return true;
+            else return false;
+        }
     }
 }
