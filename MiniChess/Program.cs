f5f34198ed19051dc7f16fc082795d12a11d47a0
João Pedro Sá Medeiros <jpsmedeiros@id.uff.br>
2018-05-01 22:49:26 -0300

    #3 Preparação para movimentar a peça -Limpeza no código -Preparação do input para ser passado para o método que movimenta a peça

--- MiniChess/Program.cs
+++ MiniChess/Program.cs
@@ -8,25 +8,17 @@ namespace MiniChess
     {
         public static int currentPlayer;
         private static bool game;
+
+        public static char[,] board;
         static void Main(string[] args)
         {
-            char[,] board = initializeGame();
-            String input = "";
-            do{
-                Console.WriteLine("Digite:\n 1- Jogar\n 2- Ajuda\n 3- Creditos\n 4- Sair\n");
-                input = Console.ReadLine();
-            } while(handleOptionInput(input));
+            board = initializeGame();
+            menuInterface();
 
             while(game){
-                Console.WriteLine("Movimente sua peça:\n");
-                input = Console.ReadLine();
-                Console.WriteLine(handleMovementInput(input)[2]);
-                input = "";
+                movementInterface();
                 game = false;
             }
-            //int cp = 1;
-            //int[] lm = { 3, 2, 3, 3 };
-            //State inicial = new State(board, cp, lm);
             printBoard(board);
         }
         
@@ -50,6 +42,22 @@ namespace MiniChess
             }
             fillPieces(board);
             return board;
+        }
+
+        private static void menuInterface(){
+            String input;
+            do{
+                Console.WriteLine("Digite:\n 1- Jogar\n 2- Ajuda\n 3- Creditos\n 4- Sair\n");
+                input = Console.ReadLine();
+            } while(handleOptionInput(input));
+        }
+
+        private static void movementInterface(){
+            String input;
+            do{
+                Console.WriteLine("Movimente sua peça:\n");
+                input = Console.ReadLine();
+            }while(handleMovementInput(input));
         }
 
         public static bool handleOptionInput(String input){
@@ -77,10 +85,29 @@ namespace MiniChess
             return false;
             
         }
-        public static string[] handleMovementInput(String input){
+        public static bool handleMovementInput(String input){
+            //TODO Verificar se formato da string está incorreto
             string[] answer;
             answer = input.Split(" ");
-            return answer;
+            if(answer.Length < 4){
+                Console.WriteLine("Movimento em formato incorreto, tente novamente no formato x1 y1 x2 y2\n");
+                return true;
+            }
+            int[] coordinates = new int[4];
+            try{
+                for(int i=0;i<answer.Length;i++){
+                        coordinates[i] = Int32.Parse(answer[i]);
+                }
+            }catch(System.FormatException){
+                Console.WriteLine("Movimento em formato incorreto, tente digitar apenas números separados por espaços\n");
+            }
+            //TODO Consultar máquina de regras if(ruleMachine.verify(answer, currentPlayer)) ... 
+            movePiece(coordinates);
+            return false;
+        }
+
+        public static void movePiece(int[] coordinates){
+            
         }
 
         public static char[,] fillPieces(char[,] board){//prenche as peças nas suas posições iniciais do jogo
